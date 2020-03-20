e8c53eba788acfb9d70b9d6e95c972dab587c729
kningtg <kningtg@gmail.com>
2020-03-20 23:05:53 +0800

    multi-label cldnn

--- model.py
+++ model.py
@@ -12,13 +12,14 @@ class ConvBlock(nn.Module):
             self.activation = nn.ReLU()
         else:
             self.activation = nn.GELU();
-        self.drop = nn.Dropout2d(dropout_rate)
+        self.dropout_rate =dropout_rate
 
     def forward(self, x):
         x = self.pad(x)
         x = self.conv(x)
         x = self.activation(x)
-        x = self.drop(x)
+        # use upper part to control the dropout function
+        x = F.dropout(x, training=self.training, p = self.dropout_rate)
         return x
 
 class MultiCLDNN(nn.Module):
@@ -28,9 +29,14 @@ class MultiCLDNN(nn.Module):
         self.conv1 = ConvBlock(1, 50, kernel_size, 0.5)
         self.conv2 = ConvBlock(50, 50, kernel_size, 0.5)
         self.conv3 = ConvBlock(50, 50, kernel_size, 0.5)
+        self.training = False
 
     def forward(self, x):
-        # (batch, channel, height, width)
+        # <type: tuple>: (batch, channel, height, width)
+        #conv1, conv2, conv3 include dropout part
+        self.conv1.training = self.training
+        self.conv2.training = self.training
+        self.conv3.training = self.training
         x1 = self.conv1(x)
         x2 = self.conv2(x1)
         x3 = self.conv3(x2)
@@ -40,7 +46,6 @@ class MultiCLDNN(nn.Module):
         batch_size = x4.shape[0]
         time_step = x4.shape[1]
         x4 = x4.reshape((batch_size, time_step, -1))
-        import ipdb; ipdb.set_trace()
         input_dim = x4.shape[1]
         lstm_out = nn.LSTM(input_size=input_dim, hidden_size=50, batch_first=True)(x4)
         lstm_out = nn.Sigmoid(lstm_out)
